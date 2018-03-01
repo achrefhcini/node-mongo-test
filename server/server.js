bc495b9015ac7bd8d1a163c3a1c1246c18234a21
achrefhcini <achref.hcini@esprit.tn>
2018-03-01 02:04:12 +0100

    Init mongoose node Api

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var bordyParser = require('body-parser')
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+
+var app=express();
+
+app.use(bordyParser.json());
+app.post('/todos',(req,res)=>{
+  var todo = new Todo({
+      text:req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+     res.status(400).send(e);
+  }); 
+})
+
+
+app.listen(3000,()=>{
+    console.log('started on port 3000')
+})
